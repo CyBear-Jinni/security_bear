8e9d6b638fd52375a6ce99f95f40c695d8968ca3
guy <guyluz11@gmail.com>
2020-10-28 15:18:57 +0200

    Fix test, add lint package

--- SecurityBearDart/bin/main.dart
+++ SecurityBearDart/bin/main.dart
@@ -1,8 +1,14 @@
-import '../lib/director.dart';
-
+import 'package:SecurityBearDart/director.dart';
 
 main(List<String> arguments) async {
   print("helllow");
-  String snapPath = arguments[0];
+
+  String snapPath;
+
+  if (arguments.length < 1) {
+    snapPath = '.';
+  } else {
+    snapPath = arguments[0];
+  }
   Director director = Director(snapPath);
 }
--- SecurityBearDart/test/network_manager_dart_test.dart
+++
@@ -1,8 +0,0 @@
-//import 'package:SecurityBearDart/network_manager_dart.dart';
-//import 'package:test/test.dart';
-//
-//void main() {
-//  test('calculate', () {
-//    expect(calculate(), 42);
-//  });
-//}
